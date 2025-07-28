9cb8b4db06fa2cf85256738e2ac7570ffa5f7410
7i6ht <ruhstals@student.ethz.ch>
2025-07-28 21:38:23 +0200

    Change file extension

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js',
+    postcss: './postcss.config.mjs',
   },
   resolve: {
     alias: {
